885ba8143103c8a636768cf4054df0ac20631119
Ross-hasee <2464480545@qq.com>
2018-04-08 21:23:36 +0800

    spring商品 后台跳转

--- src/main/java/org/rainbow/controller/CommodityController.java
+++ src/main/java/org/rainbow/controller/CommodityController.java
@@ -19,4 +19,15 @@ public class CommodityController {
 	public String index(){
 		return "Node/index";
 	}
+
+	@RequestMapping("/{page}.html")
+	public String page(@PathVariable("page") String page){
+		return "Node/" +page;
+	}
+
+	/*@RequestMapping("/add.html")
+	public String initAddPage(){
+		return "Node/add";
+	}*/
+
 }
