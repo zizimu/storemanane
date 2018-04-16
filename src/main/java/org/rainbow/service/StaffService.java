5ef4b7e2622ef7b08987917cbff404f71e37f5a9
zyw <1114972883@qq.com>
2018-04-16 17:07:49 +0800

    temp

--- src/main/java/org/rainbow/service/StaffService.java
+++ src/main/java/org/rainbow/service/StaffService.java
@@ -5,15 +5,15 @@ import java.util.List;
 import org.rainbow.pojo.TbStaff;
 
 public interface StaffService {
-	
+
 	int addStaff(TbStaff staff);
-	
-	List<TbStaff>getAllGoods();
-	
+
+	List<TbStaff> getAllGoods();
+
 	TbStaff getStaffById(long ID);
-	
+
 	int updateGoods(TbStaff staff);
-	
+
 	List<TbStaff> searchStaff(String key);
 
 }
--- src/main/java/org/rainbow/service/impl/StaffServiceImpl.java
+++ src/main/java/org/rainbow/service/impl/StaffServiceImpl.java
@@ -7,39 +7,40 @@ import org.rainbow.pojo.TbStaff;
 import org.rainbow.service.StaffService;
 import org.springframework.beans.factory.annotation.Autowired;
 
-public class StaffServiceImpl implements StaffService{
-	
+public class StaffServiceImpl implements StaffService {
+
 	@Autowired
 	private TbStaffMapper tbStaffMapper;
 
 	@Override
 	public int addStaff(TbStaff staff) {
-		
+
 		return tbStaffMapper.insertSelective(staff);
 	}
 
 	@Override
 	public List<TbStaff> getAllGoods() {
-		
+
 		return tbStaffMapper.selectAll();
 	}
 
 	@Override
 	public TbStaff getStaffById(long ID) {
-		
+
 		return tbStaffMapper.selectByPrimaryKey(ID);
 	}
 
 	@Override
 	public int updateGoods(TbStaff staff) {
-		
+
 		return tbStaffMapper.updateByPrimaryKeySelective(staff);
 	}
 
 	@Override
 	public List<TbStaff> searchStaff(String key) {
-	
+
 		return tbStaffMapper.searchStaff(key);
+
 	}
 
 }
